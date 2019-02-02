f3f4ab40f9513eb924cfdbe1ff9ab5a54e1d0ba2
Glenn <hazelnutvt04@gmail.com>
2019-02-01 19:55:46 -0500

    Added weights to the bezier function. Most descriptions of Bezier curvers GLOSS OVER what they mean by control points. You have to use the Bernstein polynomial for x AND y. The weights for the x polynomial are the x values; the weights for the y polynomial the y values

--- main.cpp
+++ main.cpp
@@ -4,25 +4,17 @@
 #include <math.h>
 #include <string>
 #include <vector>
+#include <chrono>
+#include <ctime>
 
 using std::cout;
 using std::endl;
 using std::vector;
+using std::time_t;
 
 typedef vector<vector<int>> VoV;
 
 int binomial( int n_val, int k_val ) {
-#if(0)
-   VoV lut{
-      {1}, 
-      {1,1}, 
-      {1,2,1},
-      {1,3,3,1}, 
-      {1,4,6,4,1}, 
-      {1,5,10,10,5,1}, 
-      {1,6,15,20,15,6,1}
-   };
-#endif
    VoV lut;
    while( n_val >= lut.size() ) {
       int lut_size = lut.size() ;
@@ -37,103 +29,68 @@ int binomial( int n_val, int k_val ) {
       lut.push_back(next_row);
    }
    return lut[n_val][k_val];
-#if(0)
-   int const lookup_table[7] = {  
-      {1}, 
-      {1,1}, 
-      {1,2,1},
-      {1,3,3,1}, 
-      {1,4,6,4,1}, 
-      {1,5,10,10,5,1}, 
-      {1,6,15,20,15,6,1}
-   }; 
-   while( n_val >= lookup_table.length ) {
-      int s_size = lookup_table.length;
-      nextRow = new array(s_size+1);
-      nextRow[0] = 1;
-      int prev = s_size - 1;
-      for(int index = 1; index < s_size; index++) {
-         nextRow[index] = lookup_table[prev][index-1] + 
-            lookup_table[prev][index];
-      }
-      nextRow[s_size] = 1;
-      lookup_table.add(nextRow);
-   }
-  return lookup_table[n_val][k_val]; 
-#endif
 }
 
-// TODO : Switch to memoized factorial
-int factorial( int num ) {
-   int result = 1;
-   for ( int index = 1; index <= num; index++ ) {
-      result = result * index;
-   }
-   return result;
-}
-
-double bezier( int num, double t ) {
+double bezier_basis( int num, double t_val, int weights[] ) {
    double sum = 0.0;
-   int num_fact = factorial( num );
    for ( int index = 0; index < num; index++ ) {
-      // TODO : Switch to memoized factorial
-      int index_fact = factorial( index );
-      int other_fact = factorial( num - index );
-      int temp_fact = index_fact * other_fact;
-      
-      sum += (num_fact/temp_fact) * (pow((double)(1-t),(double)(num-index))) * (pow(t,(double)index));
-      //cout << index << ": sum is " << sum << endl; 
+      sum += weights[index] * binomial(num,index) * 
+         (pow((double)(1-t_val),(double)(num-index))) * 
+         (pow(t_val,(double)index));
+
+      cout << index << ": sum is " << sum << endl; 
    }
    return sum;
 }
 
+double bezier_basis_order_2( double t_val, int weights[] ) {
+   double t_squared = t_val * t_val;
+   double mt = 1 - t_val;
+   double mt_squared = mt * mt;
+   return mt_squared * weights[0] + 
+      ( 2 * mt * t_val ) * weights[1] + 
+      t_squared * weights[2];
+}
+
+double bezier_basis_order_3( double t_val, int weights[] ) {
+   double t_squared = t_val * t_val;
+   double t_cubed = t_squared * t_val;
+   double mt = 1- t_val;
+   double mt_squared = mt * mt;
+   double mt_cubed = mt_squared * mt;
+   return mt_cubed * weights[0] + 
+      ( 2 * mt_squared * t_val ) * weights[1] + 
+      ( 3 * mt * t_squared ) * weights[2] + 
+      t_cubed * weights[3];
+}
 
 int main( int argc, char* argv[] ) {
-   
-   int n_val;
-   if ( argc> 1 ) {
-      n_val = std::stoi(argv[1],nullptr);
-   } else {
-      n_val = 1;
-   }
+   double result;
 
-   for ( int k_val = 0; k_val <= n_val; k_val++ ) {
-      int result = binomial( n_val, k_val );
-      cout << "binomial(" << n_val << "," << k_val << ") is " 
-         << result << "." << endl;
+   int x_values[] = { 120, 35, 220, 220 };
+   int y_values[] = { 160, 200, 260, 40 };
+
+   double x_results[ 200 ];
+   double y_results[ 200 ];
+   
+   clock_t start_time = clock();
+   int scaled_index = 0;
+   for ( double t_index = 0.0; t_index <= 1.000; t_index += 0.005 ) {
+      x_results[ scaled_index ] = bezier_basis_order_3( t_index, x_values );
+      cout << "bezier_basis_order_3(" << t_index << ", x ) is " 
+         << x_results[ scaled_index ] << endl; 
+      y_results[ scaled_index ] = bezier_basis_order_3( t_index, y_values );
+      cout << "bezier_basis_order_3(" << t_index << ", y ) is " 
+         << y_results[ scaled_index ] << endl; 
+      scaled_index++;
    }
+   cout << endl; 
+   double duration = (double)( clock() - start_time )/
+      (double)( CLOCKS_PER_SEC );
+   
+   cout << "Overal duration was " << duration 
+      << " seconds." << endl;
    cout << endl;
 
-
-#if(0)
-   for ( double t_index = 0.0; t_index <= 1.000; t_index += 0.005 ) {
-      double result = bezier( 3, t_index );
-      cout << "bezier(3," << t_index << " is " << result << endl; 
-   }
-
-   double result[10][2000];
-   
-   for ( double index = 0.0; index <= 1.000; index += 0.005 ) {
-      int index_adjusted = (index)/0.005;
-      cout << "Index adjusted is " << index_adjusted << endl;
-      result[0][index_adjusted] = 1;
-      result[1][index_adjusted] = (1-index);
-      result[2][index_adjusted] = index;
-      result[3][index_adjusted] = (1-index)*(1-index);
-      result[4][index_adjusted] = 2*(1-index)*index;
-      result[5][index_adjusted] = index*index;
-      result[6][index_adjusted] = (1-index)*(1-index)*(1-index);
-      result[7][index_adjusted] = 3*(1-index)*(1-index)*index;
-      result[8][index_adjusted] = 3*(1-index)*index*index;
-      result[9][index_adjusted] = index*index*index;
-   }
-   
-   for (int outer_index=0; outer_index < 10; outer_index++) {
-      for(int inner_index=0; inner_index < 200; inner_index++) {
-         cout << "Result[" << outer_index << "][" << inner_index << "]: " 
-               << result[outer_index][inner_index] << endl;
-      }
-   }
-#endif
    return 0;
 }
